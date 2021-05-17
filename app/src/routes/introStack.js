33acdd48db1da8d4d48e058bcdda5fbd87a04aa7
Donovan Andrews <Donovanandrews300@gmail.com>
2021-05-16 20:29:22 -0400

    Changed Camera filename

--- app/src/routes/introStack.js
+++ app/src/routes/introStack.js
@@ -4,7 +4,7 @@ import Introduction from '../screens/Introduction/Introduction';
 import Home from '../screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from '../screens/Login'
-import AppCamera from '../screens/Camera';
+import AppCamera from '../screens/AppCamera';
 
 
 
