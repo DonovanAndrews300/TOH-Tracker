e37b3e4f5c17ee74153e2bdda373a8188d116342
Donovan Andrews <Donovanandrews300@gmail.com>
2021-05-28 23:40:02 -0400

    Created userProfileScreen

--- app/src/routes/sideBarDrawer.js
+++ app/src/routes/sideBarDrawer.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import Home from '../screens/Home';
-import UserProfile from '../screens/UserProfile';
+import {Share, StyleSheet} from 'react-native'
+import UserProfile from '../screens/UserProfile'
 import UserResources from '../screens/UserResources';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { colors } from '../styles';
+
+
+const styles = {
+  marginVertical: 5,
+   width:20,
+  backgroundColor:"white"
+
+}
+
+const onShare = async () => {
+  try {
+    const result = await Share.share({
+      message:
+      //This should have actual app link tp share
+        'Tree of Heaven Tracker | Help track the tree of heaven to stop lanternfly',
+    });
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        // shared with activity type of result.activityType
+      } else {
+        // shared
+      }
+    } else if (result.action === Share.dismissedAction) {
+      // dismissed
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Share" onPress={onShare} />
+    </DrawerContentScrollView>
+  );
+}
 
 const Drawer = createDrawerNavigator()
 export default function SideBarDrawer() {
   return (
-
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator  initialRouteName="Home" 
+        drawerContent={props => <CustomDrawerContent {...props} />}
+        drawerStyle={{backgroundColor:colors.theme.primary500}}
+        drawerContentOptions={{
+        activeTintColor:"white",
+        style:{styles}
+      }}
+      >
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Profile" component={UserProfile} />
-        <Drawer.Screen name="Resources" component={UserResources} />
-        {
-          // Ill put the share button here using drawer list item of whatever
-        }
       </Drawer.Navigator>
   )
 }
--- app/src/screens/UserProfile.js
+++ app/src/screens/UserProfile.js
@@ -1,12 +1,78 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import {MenuButton} from "../components/buttons/buttons"
+import { Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Image, ScrollView } from 'react-native';
+import { Icon } from 'react-native-vector-icons/FontAwesome';
+import { MenuButton } from '../components/buttons/buttons';
 
 export default function UserProfile(props) {
   return (
-      <View>
-         <MenuButton props={props}/>
-          <Text>UserProfile</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+        <ScrollView>
+        <MenuButton props={props}/>
+            <View style={{alignSelf: 'center'}}>
+                <View style={styles.profileImage}>
+                    <Image source={require("../assets/asset1.jpg")}/>
+                </View>
+            </View>
+
+            <View style={styles.infoContainer}>
+                <Text style={[styles.text, {fontWeight:"200", fontSize: 36}]}>Donny</Text>
+                <Text style={[styles.text, {fontSize: 14}]}>Brasco</Text>
+            </View>
+
+            <View style={styles.statsContainer}>
+                <View style={styles.statsBox}>
+                    <Text style={[styles.text, {fontSize:24}]}>325</Text>
+                    <Text  style={[styles.text, styles.subText]}>Trees Tracked</Text>
+                </View>
+            </View>
+        </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor:"green"
+    },
+    text:{
+        color:"#525752"
+    },
+    image: {
+        flex: 1,
+        width:"undefined",
+        height:"undefined"
+    },
+    subText:{
+        fontSize:12,
+        color:"#AEB5bc",
+        textTransform: "uppercase",
+        fontWeight:"500"
+    },
+    titleBar:{
+        flexDirection:"row",
+        justifyContent: 'space-between',
+        marginTop:24,
+        marginHorizontal:16
+    },
+    profileImage:{
+        marginTop:30,
+        width: 200,
+        height:200,
+        borderRadius:100,
+        overflow:"hidden"
+    },
+    infoContainer:{
+        alignSelf:"center",
+        alignItems: 'center',
+        marginTop:10
+    },
+    statsContainer:{ 
+        flexDirection:"row",
+    },
+    statsBox:{
+        alignItems: 'center',
+        flex:1
+    }
+})
